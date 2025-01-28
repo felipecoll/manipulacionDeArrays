c41a4c43b5d95446997319efdbd2b7ad574a3615
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-28 18:00:28 -0300

    Fin de curso - manipulacion de arrays. Ultima clase con resumen texto general

---
+++ 12-flat.js
@@ -0,0 +1,9 @@
+const matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
+  const aplanarMatriz = matriz.flat()
+
+  console.log(aplanarMatriz)
---
+++ 13-mutable.js
@@ -0,0 +1,19 @@
+const products = [
+    { title: 'Pizza', price: 121, id: '🍕' },
+    { title: 'Burger', price: 121, id: '🍔' },
+    { title: 'Hot cakes', price: 121, id: '🥞' },
+];
+
+const myProducts = [];
+
+console.log("products", products);
+console.log("myProducts", myProducts);
+console.log("-".repeat(10));
+
+const product = products.find(item => item.id === '🥞')
+if (product) {
+    myProducts.push(product);
+}
+console.log("products", products);
+console.log("myProducts", myProducts);
+console.log("-".repeat(10));
