eb62c85098c85fbcaa311ea5d3b271c88571b726
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-28 15:20:36 -0300

    Clase 6 - some

--- 05-reduce.js
+++ 05-reduce.js
@@ -5,3 +5,17 @@ const respuesta = totals.reduce((sum, element) => sum + element, 0)
 console.log(respuesta)
 
 // Reload ==>
+
+const items = [1,3,2,3, 1, 10, 3 ,2]
+
+const rta = items.reduce((obj, item) => {
+    if(!obj[item]) {
+        obj[item] = 1
+    } else {
+        obj[item] = obj[item] +1
+    }
+    return obj
+
+}, {})
+
+console.log(rta)
---
+++ 06-some.js
@@ -0,0 +1,59 @@
+const numbers = [1,2,3,4]
+
+const orders = [
+    {
+      customerName: "Nicolas",
+      total: 60,
+      delivered: true,
+    },
+    {
+      customerName: "Zulema",
+      total: 120,
+      delivered: false,
+    },
+    {
+      customerName: "Santiago",
+      total: 180,
+      delivered: true,
+    },
+    {
+      customerName: "Valentina",
+      total: 240,
+      delivered: true,
+    },
+  ]
+
+  const dates = [
+    {
+      startDate: new Date(2021, 1, 1, 10),
+      endDate: new Date(2021, 1, 1, 11),
+      title: "Cita de trabajo",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 15),
+      endDate: new Date(2021, 1, 1, 15, 30),
+      title: "Cita con mi jefe",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 20),
+      endDate: new Date(2021, 1, 1, 21),
+      title: "Cena",
+    },
+  ];
+
+const respuesta = numbers.some(item => item % 2 ===0)
+console.log(respuesta)
+
+const respuesta2 = orders.some(item => item.delivered)
+console.log(respuesta2)
+
+const respuesta3 = dates.some()
+
+const areIntervalsOverlapping = require('date-fns/areIntervalsOverlapping');
+
+const isOverlap = (newDate) => {
+  return dates.some(date => areIntervalsOverlapping(
+    { start: date.startDate, end: date.endDate },
+    { start: newDate.startDate, end: newDate.endDate },
+  )
+)}
