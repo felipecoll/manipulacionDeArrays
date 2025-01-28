a1b1a11d6620da47aadf7839d317781dbe1b8890
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-28 16:26:47 -0300

    Clase 10 - join

---
+++ 10-join.js
@@ -0,0 +1,9 @@
+const elements = ['Fire', 'Air', 'Water'];
+
+const solution = elements.join(' - ')
+
+console.log(solution)
+
+const titulo = 'Curso de manipulacion de arrays'
+
+console.log(titulo.split(' ').join('_'))
