2931d0730d63bcdbb68f882a09bfd31aa542c190
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-28 17:17:56 -0300

    Clase 11 - concat

---
+++ 11-concat.js
@@ -0,0 +1,6 @@
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+const otherAnimals = ['perro', 'gato', 'comadreja', 'vibora']
+
+const unionArrays = animals.concat(otherAnimals);
+
+console.log(unionArrays)
